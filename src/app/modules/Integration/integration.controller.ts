ad7e9e8c234866ccb2de8dc74cf2a17fefd40d47
rashedrk <rashedrk001@gmail.com>
2024-10-22 16:47:13 +0600

    redriect url fx

--- src/app/modules/Integration/integration.controller.ts
+++ src/app/modules/Integration/integration.controller.ts
@@ -2,6 +2,7 @@ import { integrationServices } from './integration.service';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { HttpStatusCode } from 'axios';
+import config from '../../config/config';
 
 const githubIntegration = catchAsync(async (req, res) => {
     const { code } = req.query;
@@ -9,7 +10,7 @@ const githubIntegration = catchAsync(async (req, res) => {
     const response = await integrationServices.githubIntegration(code as string)
 
     if (response) {
-        res.redirect(`http://localhost:3000/issue?id=${response?.integrationId}`);
+        res.redirect(`${config.base_url}/issue?id=${response?.integrationId}`);
     }
     else if (response === false) {
         res.status(500).json({ error: 'Error during GitHub App installation' });
